e55f49820eee692fd256e3263bf9503cdaedd4d7
davidnicholastaylor <dntaylor14@gmail.com>
2018-08-01 15:35:27 -0500

    bologna

--- js/parkInfo.js
+++ js/parkInfo.js
@@ -2,11 +2,14 @@
 
 let db = require('./db-calls.js');
 
+
 let parkInfo = {};
 
 db.fetchParkInfo()
    .then((result) => {
    parkInfo = result;
-});
+   })
+
+// console.log("park info db call", parkInfo);
 
 module.exports = parkInfo;
